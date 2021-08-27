cfe327c652c1d7a07538114a72916f0b944a06f3
baluha <romanbalushka@gmail.com>
2021-08-27 16:59:27 +0300

    5.1.4. FlatMap для Iterator<Iterator>

--- src/main/java/ru/job4j/it/MatrixIt.java
+++ src/main/java/ru/job4j/it/MatrixIt.java
@@ -14,7 +14,8 @@ public class MatrixIt implements Iterator<Integer> {
 
     @Override
     public boolean hasNext() {
-        while ((data[row].length == 0 || column >= data[row].length) && row < data.length - 1) {
+        while ((data[row].length == 0 || column >=
+                data[row].length) && row < data.length - 1) {
             row++;
             column = 0;
         }

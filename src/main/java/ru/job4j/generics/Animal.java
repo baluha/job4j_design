4dea01b425962eb8a9c2f1d35fe6519a5ed6ed64
baluha <romanbalushka@gmail.com>
2021-09-02 13:51:26 +0300

    0. Что такое обобщенные типы (generics)

---
+++ src/main/java/ru/job4j/generics/Animal.java
@@ -0,0 +1,21 @@
+package ru.job4j.generics;
+
+public class Animal {
+    private int age;
+    private String sex;
+
+
+    public Animal(int age, String sex) {
+        this.age = age;
+        this.sex = sex;
+    }
+
+
+    @Override
+    public String toString() {
+        return "Animal{" +
+                "age=" + age +
+                ", sex='" + sex + '\'' +
+                '}';
+    }
+}

53cdfc0e57857408a6bad673d92898b60ff9b140
baluha <romanbalushka@gmail.com>
2021-09-13 10:37:50 +0300

    0. Что такое обобщенные типы (generics)

--- src/main/java/ru/job4j/generics/Generics.java
+++ src/main/java/ru/job4j/generics/Generics.java
@@ -1,4 +1,4 @@
-package ru.job4j.generic;
+package ru.job4j.generics;
 
 import java.util.ArrayList;
 import java.util.Iterator;
@@ -9,10 +9,10 @@ public class Generics {
         Generics gen = new Generics();
         List<Animal> first = new ArrayList<>();
         List<Predator> second = new ArrayList<>();
-        List<Animal> third = new ArrayList<>();
-        first.add(new Animal(999999, "default"));
-        second.add(new Predator(90000, "default"));
-        third.add(new Tiger(5, "male"));
+        List<Tiger> third = new ArrayList<>();
+        first.add(new Animal());
+        second.add(new Predator());
+        third.add(new Tiger());
 
         gen.printObject(first);
         gen.printObject(second);
@@ -21,12 +21,12 @@ public class Generics {
 
 //        gen.printBoundedWildCard(first);
         gen.printBoundedWildCard(second);
-//        gen.printBoundedWildCard(third);
+        gen.printBoundedWildCard(third);
         System.out.println();
 
         gen.printLowerBoundedWildCard(first);
         gen.printLowerBoundedWildCard(second);
-        gen.printLowerBoundedWildCard(third);
+//        gen.printLowerBoundedWildCard(third);
     }
 
     public void printObject(List<?> list) {

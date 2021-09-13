d13200e40d1ce57de81cecd9be06349350373547
baluha <romanbalushka@gmail.com>
2021-09-13 10:39:00 +0300

    0. Что такое обобщенные типы (generics)

--- src/main/java/ru/job4j/generic/Predator.java
+++
@@ -1,7 +0,0 @@
-package ru.job4j.generic;
-
-public class Predator extends Animal {
-    public Predator(int age, String sex) {
-        super(age, sex);
-    }
-}
--- src/main/java/ru/job4j/generic/Tiger.java
+++
@@ -1,7 +0,0 @@
-package ru.job4j.generic;
-
-public class Tiger extends Predator {
-    public Tiger(int age, String sex) {
-        super(age, sex);
-    }
-}

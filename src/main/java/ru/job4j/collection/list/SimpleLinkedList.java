8c4e4af1029763034298eacc9c2249ee14880713
baluha <romanbalushka@gmail.com>
2021-09-14 14:39:53 +0300

    2. Создать контейнер на базе связанного списка

---
+++ src/main/java/ru/job4j/collection/list/SimpleLinkedList.java
@@ -0,0 +1,96 @@
+package ru.job4j.collection.list;
+
+import java.util.ConcurrentModificationException;
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+import java.util.Objects;
+
+public class SimpleLinkedList<E> implements List<E> {
+
+    private int size;
+    private int modCount;
+    private Node<E> first;
+    private Node<E> last;
+
+
+    @Override
+    public void add(E value) {
+        Node<E> l = last;
+        Node<E> newNode = new Node<>(l, value, null);
+        last = newNode;
+        if (first == null) {
+            first = newNode;
+        } else {
+            this.last.prev = first;
+            first.next = last;
+        }
+        size++;
+        modCount++;
+    }
+
+    @Override
+    public E get(int index) {
+        Objects.checkIndex(index, size);
+        Node<E> nodeRsl;
+        if (index == 0) {
+            return first.item;
+        }
+        else if(index == size - 1) {
+            return last.item;
+        } else {
+            nodeRsl = first;
+            for (int i = 0; i < size; i++) {
+                nodeRsl = nodeRsl.next;
+                if (i == index) {
+                    return nodeRsl.item;
+                }
+            }
+        }
+        return null;
+    }
+
+    @Override
+    public Iterator<E> iterator() {
+
+
+        return new Iterator<>() {
+
+            private int count = 0;
+            final int sizeOfList = size;
+            private Node<E> cursor = first;
+            final int exp = modCount;
+
+            @Override
+            public boolean hasNext() {
+                if (exp != modCount) {
+                    throw new ConcurrentModificationException();
+                }
+                return count < sizeOfList;
+            }
+
+            @Override
+            public E next() {
+                if (!hasNext()) {
+                    throw new NoSuchElementException();
+                }
+                E rsl = cursor.item;
+                cursor = cursor.next;
+                count++;
+                return rsl;
+            }
+        };
+    }
+
+
+    private static class Node<E> {
+        E item;
+        Node<E> next;
+        Node<E> prev;
+
+        Node(Node<E> prev,E element, Node<E> next) {
+            this.prev = prev;
+            this.item = element;
+            this.next = next;
+        }
+    }
+}

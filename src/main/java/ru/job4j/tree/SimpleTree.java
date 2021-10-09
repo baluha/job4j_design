59808df22bac9e98b950240eb90414e2d2e0b61d
baluha <romanbalushka@gmail.com>
2021-10-09 20:39:44 +0300

    2. Добавить метод boolean isBinary()

--- src/main/java/ru/job4j/tree/SimpleTree.java
+++ src/main/java/ru/job4j/tree/SimpleTree.java
@@ -1,6 +1,7 @@
 package ru.job4j.tree;
 
 import java.util.*;
+import java.util.function.Predicate;
 
 public class SimpleTree<E> implements Tree<E> {
     private final Node<E> root;
@@ -15,9 +16,9 @@ public class SimpleTree<E> implements Tree<E> {
         Optional<Node<E>> par = findBy(parent);
         Optional<Node<E>> chil = findBy(child);
         if (par.isPresent() && chil.isEmpty())  {
-            Node<E> newChild = new Node<>(child);
-            par.get().children.add(newChild);
-            rsl = true;
+                Node<E> newChild = new Node<>(child);
+                par.get().children.add(newChild);
+                rsl = true;
         }
 
         return rsl;
@@ -25,22 +26,25 @@ public class SimpleTree<E> implements Tree<E> {
 
     @Override
     public Optional<Node<E>> findBy(E value) {
+        return findByPredicate(e -> e.value.equals(value));
+    }
+
+    public boolean isBinary() {
+        return findByPredicate(e -> e.children.size() > 2).isEmpty();
+    }
+
+    private Optional<Node<E>> findByPredicate(Predicate<Node<E>> condition) {
         Optional<Node<E>> rsl = Optional.empty();
         Queue<Node<E>> data = new LinkedList<>();
         data.offer(this.root);
         while (!data.isEmpty()) {
             Node<E> el = data.poll();
-            if (el.value.equals(value)) {
+            if (condition.test(el)) {
                 rsl = Optional.of(el);
                 break;
             }
             data.addAll(el.children);
         }
         return rsl;
-    }
-
-    @Override
-    public boolean isBinary() {
-        return false;
     }
 }

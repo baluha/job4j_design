2da3277926e6083b1965a50e451314297acf09ac
baluha <romanbalushka@gmail.com>
2021-10-15 20:56:28 +0300

    2. Анализ доступности сервера.

--- src/main/java/ru/job4j/io/Analizy.java
+++ src/main/java/ru/job4j/io/Analizy.java
@@ -16,14 +16,12 @@ public class Analizy {
                 String[] str = l.split(" ");
                 if ((str[0].equals("400") || str[0].equals("500")) && lst.size() == 0) {
                     lst.add(str[1]);
-                }
-                else if ((str[0].equals("200") || str[0].equals("300")) && lst.size() == 1) {
+                } else if ((str[0].equals("200") || str[0].equals("300")) && lst.size() == 1) {
                     lst.add(str[1]);
                     st.append(lst.get(0)).append(";").append(lst.get(1)).append(";").append(System.lineSeparator());
                     lst.clear();
                 }
             });
-
             out.write(st.toString());
         } catch (Exception e) {
             e.printStackTrace();

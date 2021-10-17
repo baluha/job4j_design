5438ef0873760a9e01403272ae68f52c10612f4a
baluha <romanbalushka@gmail.com>
2021-10-17 15:31:45 +0300

    3.0. Тестирование IO

---
+++ src/test/java/ru/job4j/io/AnalizyTest.java
@@ -0,0 +1,38 @@
+package ru.job4j.io;
+
+import static org.junit.Assert.assertThat;
+import static org.hamcrest.Matchers.is;
+
+import junit.framework.TestCase;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
+
+import java.io.*;
+
+public class AnalizyTest{
+    @Rule
+    public TemporaryFolder folder = new TemporaryFolder();
+
+    @Test
+    public void test() throws IOException {
+        File source = folder.newFile("source.txt");
+        File target = folder.newFile("target.csv");
+        try (PrintWriter out = new PrintWriter(source)) {
+            out.println("200 10:56:01");
+            out.println("400 10:57:01");
+            out.println("500 10:58:01");
+            out.println("500 10:59:01");
+            out.println("500 11:00:01");
+            out.println("200 11:01:01");
+            out.println("200 11:02:01");
+        }
+        Analizy analizy = new Analizy();
+        analizy.unavailable(source.getAbsolutePath(), target.getAbsolutePath());
+        StringBuilder rsl = new StringBuilder();
+        try (BufferedReader in = new BufferedReader(new FileReader(target))) {
+            in.lines().forEach(rsl::append);
+        }
+        assertThat(rsl.toString(), is("10:57:01;11:01:01;"));
+    }
+}

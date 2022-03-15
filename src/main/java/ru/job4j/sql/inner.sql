31e5ebbc4b3d90eb48e1aa82abd8f3e6eda68e8c
baluha <romanbalushka@gmail.com>
2022-03-15 20:23:38 +0300

    1. Inner join

---
+++ src/main/java/ru/job4j/sql/inner.sql
@@ -0,0 +1,36 @@
+create table shop (
+id serial primary key,
+	name varchar(64)
+);
+
+create table cats (
+name varchar(64),
+	 age int,
+	shop_id int references shop(id)
+);
+
+insert into shop(name) values('shop1');
+insert into shop(name) values('shop2');
+insert into shop(name) values('shop3');
+insert into shop(name) values('shop4');
+
+insert into cats(name, age, shop_id) values('cat1', 4, 1);
+insert into cats(name, age, shop_id) values('cat2', 2, 1);
+insert into cats(name, age, shop_id) values('cat3', 3, 2);
+insert into cats(name, age, shop_id) values('cat4', 8, 4);
+insert into cats(name, age, shop_id) values('cat5', 5, 4);
+
+SELECT ct.name, sh.name
+from cats as ct
+join shop as sh
+on ct.shop_id = sh.id;
+
+SELECT ct.name, ct.age, sh.name
+from cats as ct
+join shop as sh
+on ct.shop_id = sh.id;
+
+SELECT ct.name, ct.age, sh.name, sh.id
+from cats as ct
+join shop as sh
+on ct.shop_id = sh.id;
